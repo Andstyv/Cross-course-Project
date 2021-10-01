c9c4a853d3928d3e2cd233aaf098fa8870fef67a
Andstyv <74601625+Andstyv@users.noreply.github.com>
2021-10-01 12:35:31 +0200

    Update productFetcher.js

--- js/productFetcher.js
+++ js/productFetcher.js
@@ -2,7 +2,6 @@ const productURL = "https://cms-ca.styve.digital/wp-json/wc/store/products";
 const productsContainer = document.querySelector(".products-grid");
 const cartItems = JSON.parse(localStorage.getItem("cartList"));
 const numberOfItems = document.querySelector(".numberOfItems");
-const numberOfItems2 = document.querySelector(".numberOfItems2");
 
 async function getProducts(url) {
   const response = await fetch(url);
