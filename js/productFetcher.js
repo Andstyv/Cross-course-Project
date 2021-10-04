10264f9ae767ba3689431b07c0423b6ab4146185
Andstyv <74601625+Andstyv@users.noreply.github.com>
2021-10-04 14:47:41 +0200

    Added search functionality

--- js/productFetcher.js
+++ js/productFetcher.js
@@ -6,6 +6,10 @@ const numberOfItems = document.querySelector(".numberOfItems");
 const catMenBtn = document.getElementById("cat-men-btn");
 const catWomenBtn = document.getElementById("cat-women-btn");
 const catKidsBtn = document.getElementById("cat-kids-btn");
+const catResetBtn = document.getElementById("cat-reset-btn");
+
+const clearSearchBtn = document.querySelector(".search-clear");
+const searchBtn = document.querySelector(".searchButton");
 
 const catMenURL = "https://cms-ca.styve.digital/wp-json/wc/store/products?category=16";
 const catWomenURL = "https://cms-ca.styve.digital/wp-json/wc/store/products?category=17";
@@ -15,12 +19,12 @@ async function getProducts(url) {
   const response = await fetch(url);
   const products = await response.json();
 
-  console.log(cartItems.length);
-  productsContainer.innerHTML = "";
-  products.forEach(function (product) {
-    productsContainer.innerHTML += `<div class="product-card"> 
+  if (products.length) {
+    productsContainer.innerHTML = "";
+    products.forEach(function (product) {
+      productsContainer.innerHTML += `<div class="product-card"> 
     <a href="product_specific.html?id=${product.id}">
-    <img src="${product.images[0].src}" alt="${product.images.alt}" class="jacketimg">
+    <img src="${product.images[0].src}" alt="${product.name}" class="jacketimg">
     </a>
     <div class="prod-text"> 
     <h2 class="product-title">${product.name}</h2>
@@ -28,7 +32,10 @@ async function getProducts(url) {
     ${product.description}</div>
     <a href="product_specific.html?id=${product.id}" class="prod-explore">EXPLORE</a>
     </div></div>`;
-  });
+    });
+  } else {
+    productsContainer.innerHTML = `<div class="no-products">The search returned no results.</div>`;
+  }
 }
 getProducts(productURL);
 
@@ -48,6 +55,7 @@ catMenBtn.addEventListener("click", function () {
   catWomenBtn.style.background = "none";
   catKidsBtn.style.color = "#fff";
   catKidsBtn.style.background = "none";
+  document.querySelector(".searchWord").value = "";
 });
 
 catWomenBtn.addEventListener("click", function () {
@@ -58,6 +66,7 @@ catWomenBtn.addEventListener("click", function () {
   catKidsBtn.style.background = "none";
   catMenBtn.style.color = "#fff";
   catMenBtn.style.background = "none";
+  document.querySelector(".searchWord").value = "";
 });
 
 catKidsBtn.addEventListener("click", function () {
@@ -68,4 +77,44 @@ catKidsBtn.addEventListener("click", function () {
   catMenBtn.style.background = "none";
   catWomenBtn.style.color = "#fff";
   catWomenBtn.style.background = "none";
+  document.querySelector(".searchWord").value = "";
+});
+
+catResetBtn.addEventListener("click", function () {
+  getProducts(productURL);
+  catKidsBtn.style.color = "#fff";
+  catKidsBtn.style.background = "none";
+  catMenBtn.style.color = "#fff";
+  catMenBtn.style.background = "none";
+  catWomenBtn.style.color = "#fff";
+  catWomenBtn.style.background = "none";
+  catResetBtn.style.color = "#fff";
+  catResetBtn.style.background = "none";
+  document.querySelector(".searchWord").value = "";
+});
+
+function searchProducts() {
+  const searchInput = document.querySelector(".searchWord").value;
+  const searchURL = productURL + `?search=${searchInput}`;
+
+  getProducts(searchURL);
+}
+
+searchBtn.addEventListener("click", function () {
+  if (document.querySelector(".searchWord").value) {
+    searchProducts();
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" && document.querySelector(".searchWord").value) {
+    searchProducts();
+  }
+});
+
+clearSearchBtn.addEventListener("click", function () {
+  document.querySelector(".searchWord").value = "";
+  clearSearchBtn.style.color = "#fff";
+  clearSearchBtn.style.background = "none";
+  getProducts(productURL);
 });
